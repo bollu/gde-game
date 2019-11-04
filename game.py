feae1375a670e81a56d3932defe01675bf85d0c3
Siddharth Bhat <siddu.druid@gmail.com>
2019-11-05 05:12:53 +0530

    setup vader for words

--- game.py
+++ game.py
@@ -3,12 +3,38 @@ import curses
 from curses import wrapper
 from curses.textpad import Textbox, rectangle
 from curses import ascii
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer 
+
+
+
+PAIR_RED_ON_WHITE = 1
+PAIR_GREEN_ON_WHITE = 2
+
+sid_obj = SentimentIntensityAnalyzer() 
+
 
 def print_prompt(pad, s):
     pad.clear()
     pad.addch(0, ord('>'))
-    for c in s:
-        pad.addch(0, ord(c))
+    blob = TextBlob(s)
+
+    words = s.split(' ')
+    for i, w in enumerate(words):
+        senti = sid_obj.polarity_scores(w)
+        if senti['neg'] >= 0.5: pad.addstr(w, curses.color_pair(PAIR_RED_ON_WHITE))
+        elif senti['pos'] >= 0.5: pad.addstr(w, curses.color_pair(PAIR_GREEN_ON_WHITE))
+        else: pad.addstr(w)
+        
+        # pad.addstr(w)
+        # pad.addstr(w, curses.COLOR_RED)
+        # pad.addstr(w, 1)
+        #for c in w:
+        #    pad.addch(0, ord(c))
+        if i < len(words)-1: pad.addch(0, ord(' '))
+
+    pad.addch(0, ord('_'))
 
 def read_input(stdscr, input_pad):
 
@@ -32,6 +58,13 @@ def main(stdscr):
     input_pad = curses.newpad(100, 100)
 
     # Clear screen
+    curses.start_color()
+    curses.use_default_colors()
+    curses.init_pair(PAIR_RED_ON_WHITE, curses.COLOR_RED, -1)
+    curses.init_pair(PAIR_GREEN_ON_WHITE, curses.COLOR_GREEN, -1)
+
+    # for i in range(0, curses.COLORS):
+    #     curses.init_pair(i + 1, i, -1)
     stdscr.clear()
     stdscr.refresh()
 
